bf36983986c9511ad4cadfcdf19782ccf7de457d
Luiz Gustavo <lgds.demtrio@gmail.com>
2024-12-01 08:47:09 -0300

    Meu primeiro desafio resolvido, e sem roubar

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
-# Refatore o exercicio 2
+"""
+1. O programa deve começar solicitando ao usuário que insira seu nome.
+2. Em seguida, o programa deve pedir ao usuário para inserir o valor do seu salário. Considere que este valor pode ser um número decimal.
+3. Depois, o programa deve solicitar a porcentagem do bônus recebido pelo usuário, que também pode ser um número decimal.
+4. O cálculo do KPI do bônus de 2024 é de 1000 + salario * bônus
+5. Finalmente, o programa deve imprimir uma mensagem no seguinte formato: "Olá [nome], o seu valor bônus foi de 5000".
+"""
 
-nome = input("Digite seu nome: ")
-quantidade_caracteres = len(nome)
-print(quantidade_caracteres)
+nome = input("Digite seu nome aqui: ")
+salario = int(input("digite seu salario: "))
+bonus = float(input("qual a porcentagem do seu bonus?: "))
+calculoKPI = int(1000 + (salario * bonus))
+mensagemfinal = print(f"Ola {nome}, o seu valor bonus foi de {calculoKPI}")
